bf3346f040f3073410b4334838aaa2f8e6ac1e2b
grachet <guillaume.rachet@gmail.com>
2018-04-03 21:43:45 +0300

    page intro prete

--- src/IUT/CoreBundle/Controller/CoreController.php
+++ src/IUT/CoreBundle/Controller/CoreController.php
@@ -6,25 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class CoreController extends Controller
 {
-    public function onepageAction()
-    {
-        return $this->render('IUTCoreBundle:Default:onepage.html.twig');
-    }
+
 
     public function homeAction()
     {
         return $this->render('IUTCoreBundle:Default:home.html.twig');
     }
 
-    public function listpersoAction()
-    {
-        return $this->render('IUTCoreBundle:Default:listperso.html.twig');
-    }
-
-    public function pagepersoAction()
-    {
-        return $this->render('IUTCoreBundle:Default:pageperso.html.twig');
-    }
 
 
 
